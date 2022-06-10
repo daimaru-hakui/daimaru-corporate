4c079ea0f15e47679caf415edbdd3ada35268d46
daimaru-hakui <dh.mukai.dh@gmail.com>
2022-06-10 16:19:13 +0900

    代表挨拶　追加

---
+++ deta.js
@@ -0,0 +1,34 @@
+export const Menus = [
+  {
+    name: "Serenade",
+    link: "/serenade",
+  },
+  {
+    name: "商品情報",
+    link: "/product",
+  },
+  {
+    name: "会社情報",
+    link: "/company",
+  },
+  {
+    name: "代表挨拶",
+    link: "/greetings",
+  },
+  // {
+  //   name: '店舗情報',
+  //   link: '/shop',
+  // },
+  // {
+  //   name: 'CSR',
+  //   link: '/csr',
+  // },
+  {
+    name: "採用情報",
+    link: "/recruit",
+  },
+  {
+    name: "お問い合わせ",
+    link: "/contact",
+  },
+];
