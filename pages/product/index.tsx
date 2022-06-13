37c3202f895d70ca42495f7eaac2287ec002f6ee
sukebon <39957486+sukebon@users.noreply.github.com>
2022-06-13 20:36:48 +0900

    product module scss

--- pages/product/index.tsx
+++ pages/product/index.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
 import Head from 'next/head';
-import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import Footer from '../../components/footer/Footer';
