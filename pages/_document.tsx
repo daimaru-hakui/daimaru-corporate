95b27a3ec19970667632cace948617f7bbaa09da
daimaru-hakui <dh.mukai.dh@gmail.com>
2022-06-17 13:55:02 +0900

    オーダーメイド画像追加

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,20 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang='ja'>
+      <Html lang="ja">
         <Head>
-          <link rel='icon' href='/favicon.ico' />
-          <link rel='preconnect' href='https://fonts.googleapis.com' />
-          <link rel='preconnect' href='https://fonts.gstatic.com' />
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            rel='stylesheet'
-            href='https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@200;300;400;500;600;700;900&display=swap'
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@200;300;400;500;600;700;900&display=swap"
+          />
+          <meta
+            name="description"
+            content="大丸白衣は食品業界向けユニフォーム、医療業界向け白衣、作業着、制服をご提案！異物混入対策や動きやすさにも対応。ユニフォームのレンタルも行っています。"
           />
         </Head>
         <body>
